39fca74980314e4eb312f74f199a7e76599fd741
Cezar Pavelski <cezarpavelski@gmail.com>
2015-09-27 15:05:42 -0300

    Adicionado mocha para os testes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 var gulp = require('gulp');
 var coffeelint = require('gulp-coffeelint');
+var Mocha = require('mocha');
 var files = "./scripts/*.coffee";
 
 gulp.task('lint', function() {
-  gulp.src(files)
+  return gulp.src(files)
     .pipe(coffeelint())
-    .pipe(coffeelint.reporter('default'));
+    .pipe(coffeelint.reporter())
+    .pipe(coffeelint.reporter('fail'));
   }
 );
 
+gulp.task('test', ['lint'], function (done) {
+  var m = new Mocha({
+    ui: 'bdd',
+    reporter: 'spec'
+  });
+  m.run(done);
+});
+
 gulp.task('default', function() {
-  gulp.start('lint');
+  gulp.start('test');
 });
